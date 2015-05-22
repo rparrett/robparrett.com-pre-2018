90e5d812087a63a4ffe2d3cbaddf428d5c4045d0
Rob Parrett <robparrett@gmail.com>
2015-05-22 20:20:43 +0000

    Cleanup nonsensical comment

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 define("START_TIME", microtime(true));
 define("APPLICATION_PATH", dirname(__FILE__));
 
-// TODO load conf for current environment
 require_once(APPLICATION_PATH . "/application/conf/conf.php");
 
 $app  = new Yaf_Application($config);
